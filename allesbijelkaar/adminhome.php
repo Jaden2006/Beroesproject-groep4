e4d59610bfd3e99abd057d6dbcad7e1b6c1de36c
VZ <131747053+VaZoi@users.noreply.github.com>
2023-06-29 15:21:09 +0200

    Add files via upload

---
+++ allesbijelkaar/adminhome.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Campingbeheer</title>
+    <link rel="stylesheet" href="beschikbaarheid.css">
+</head>
+<?php
+
+$host = 'localhost:3307';
+$db = 'Camping Village';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int) $e->getCode());
+}
+?>
+<body>
+
+    
+    <h2> Hello Admin </h2>
+
+    <h1>Campingbeheer</h1>
+
+    <form>
+        <label for="plekken">Aantal beschikbare plekken:</label>
+        <input type="number" id="plekken" name="plekken" min="0">
+        <button type="submit" id="opslaan">Opslaan</button>
+    </form>
+
+    <script>
+        // Eventlistener voor het formulier
+        document.querySelector('form').addEventListener('submit', function (event) {
+            event.preventDefault(); // Voorkomt dat de pagina wordt vernieuwd
+
+            // Haal de waarde van het inputveld op
+            var aantalPlekken = document.getElementById('plekken').value;
+
+            // Stuur de waarde naar de server (hier simuleren we het opslaan)
+            // Je kunt deze waarde vervangen door een API-aanroep om het naar een server te sturen
+            console.log('Aantal plekken opgeslagen: ' + aantalPlekken);
+        });
+    </script>
+
+    <?php
+
+$data = $pdo->query("SELECT * FROM reserveren")->fetchAll();
+
+foreach ($data as $row) {
+    echo "naam: " . $row['visitor_name'] . "<br />\n";
+    echo "email: " . $row['visitor_email'] . "<br />\n";
+    echo "telefonnnummer: " . $row['visitor_phone'] . "<br />\n";
+    echo "adults: " . $row['total_adults'] . "<br />\n";
+    echo "children: " . $row['total_children'] . "<br />\n";
+    echo "check in: " . $row['checkin_date'] . "<br />\n";
+    echo "check ot: " . $row['checkout_date'] . "<br />\n";
+    echo "message: " . $row['visitor_message'] . "<br />\n";
+    echo "-----------------------------------". "<br /><hr>";
+}
+?>
+
+<footer>
+        <div class="contact">
+            <p>Locatie: pittoreske Otterlo</p>
+            <p>Emailadres: Campingvillage@gmail.com</p>
+            <p>Telefoonnummer: 020 633782347</p>
+        </div>
+    </footer>
+</body>
+
+</html>
---
+++ allesbijelkaar/login.sql
@@ -0,0 +1,38 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3307
+-- Gegenereerd op: 29 jun 2023 om 13:53
+-- Serverversie: 10.4.14-MariaDB
+-- PHP-versie: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `admin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `login`
+--
+
+CREATE TABLE `login` (
+  `Username` varchar(20) DEFAULT NULL,
+  `Password` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
